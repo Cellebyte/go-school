7ddd80b776b59009019a6b032ed7dce4df363282
Marcel Fest <marcel.fest@telekom.de>
2024-11-14 13:40:26 +0100

    my guesser is rudimentary

--- workshop/01-getting-started/0021-number-guesser/main.go
+++ workshop/01-getting-started/0021-number-guesser/main.go
@@ -1,10 +1,40 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+)
+
 // number guessing game
 func main() {
 	// TODO: 1. generate a random number
-
+	randNumber := int64(rand.Intn(100))
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter the number: ")
+	fmt.Println("-----------------------------")
 	for {
+		fmt.Print("-> ")
+		text, _ := reader.ReadString('\n')
+		// convert CRLF to LF
+		text = strings.Replace(text, "\n", "", -1)
+		userValue, err := strconv.ParseInt(text, 10, 64)
+		if err != nil {
+			fmt.Printf("Your input is not a number %q \n", text)
+			continue
+		}
+		if userValue < randNumber {
+			fmt.Println("Too low")
+		} else if userValue > randNumber {
+			fmt.Println("Too high")
+		} else {
+			fmt.Println("You win")
+			break
+		}
+
 		// TODO: 2. read the user input
 
 		// TODO: 3. convert the user input to an integer
