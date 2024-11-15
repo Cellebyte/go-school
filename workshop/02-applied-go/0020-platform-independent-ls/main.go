aab815b33162437f5bb1bc9b569f105792822cd8
Marcel Fest <marcel.fest@telekom.de>
2024-11-15 13:40:49 +0100

    my own ls

--- workshop/02-applied-go/0020-platform-independent-ls/main.go
+++ workshop/02-applied-go/0020-platform-independent-ls/main.go
@@ -10,12 +10,28 @@ func main() {
 
 	// TODO 1:  get current directory
 	var dirPath string
-
+	dirPath, err := os.Getwd()
+	if err != nil {
+		fmt.Println("cannot find current dir directory")
+		os.Exit(1)
+	}
 	// TODO 2:  open directory and get file pointer
 	var dir *os.File
-
+	dir, err = os.Open(dirPath)
+	if err != nil {
+		fmt.Println("cannot open current directory")
+		os.Exit(1)
+	}
 	// TODO 3:  get files from directory
 	var files []os.FileInfo
+	files, err = dir.Readdir(0)
+	if err != nil {
+		fmt.Println("cannot read current directory")
+		os.Exit(1)
+	}
 
 	// TODO 4:  print file contents
+	for _, file := range files {
+		fmt.Printf("%v %d %v %s\n", file.Mode(), file.Size(), file.ModTime(), file.Name())
+	}
 }
